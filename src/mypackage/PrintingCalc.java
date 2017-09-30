3f6ce4de05312edb88c45abb043a727b8ee0b71d
superpower1 <superpiglet1@gmail.com>
2017-09-30 13:24:49 +1000

    basic structure

--- src/mypackage/PrintingCalc.java
+++ src/mypackage/PrintingCalc.java
@@ -1,9 +1,32 @@
 package mypackage;
 
 public class PrintingCalc {
+	
+	private int blackSinglePrice;
+	private int blackDoublePrice;
+	private int colorSinglePrice;
+	private int colorDoublePrice;
+	
+	PrintingCalc(int blackSinglePrice, int blackDoublePrice, int colorSinglePrice, int colorDoublePrice) {
+		this.blackSinglePrice = blackSinglePrice;
+		this.blackDoublePrice = blackDoublePrice;
+		this.colorSinglePrice = colorSinglePrice;
+		this.colorDoublePrice = colorDoublePrice;
+	}
+	
+	private void calc(int totalPage, int colorPage, boolean isDouble) {
+		
+		
+	}
+	
+	public void readFile(String filename) {
+		
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PrintingCalc A4 = new PrintingCalc(15, 10, 25, 20);
+		
 
 	}
 
